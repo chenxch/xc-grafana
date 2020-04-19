5fb58c56bf80025cf7b3c41620242bfe17e57f95
chenxch <chenxch@ffcs.cn>
2020-04-19 13:17:56 +0800

    初始化

---
+++ src/components/DashboardGrid/DashboardGrid.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="grid">
+    <div class="grid-title">
+      <div>
+        <el-checkbox v-model="draggable">draggable</el-checkbox>
+        <el-checkbox v-model="resizable">resizable</el-checkbox>
+      </div>
+      <div>Displayed as [x, y, w, h]:</div>
+      <div>
+        <div v-for="item in layout" :key="item.i" class="grid-title-item">
+          {{item.i}}:[{{item.x}},{{item.y}},{{item.w}},{{item.h}}]</div>
+      </div>
+    </div>
+    <grid-layout :layout="layout" :col-num="24" :row-height="30" :is-draggable="draggable" :is-resizable="resizable"
+      :vertical-compact="true" :margin="[10, 10]" :use-css-transforms="true">
+      <grid-item v-for="item in layout" :key="item.i" class="vue-grid-item" :x="item.x" :y="item.y" :w="item.w"
+        :h="item.h" :i="item.i">
+        <div class="item-charts">
+          <MyChart :id="item.i"></MyChart>
+          </div>
+      </grid-item>
+    </grid-layout>
+  </div>
+</template>
+<script>
+  import {
+    GridLayout,
+    GridItem
+  } from 'vue-grid-layout';
+  import MyChart from '../MyChart';
+
+  const testLayout = [{
+      "x": 0,
+      "y": 0,
+      "w": 12,
+      "h": 8,
+      "i": "0"
+    },
+    {
+      "x": 12,
+      "y": 0,
+      "w": 12,
+      "h": 8,
+      "i": "1"
+    },
+    {
+      "x": 0,
+      "y": 5,
+      "w": 24,
+      "h": 10,
+      "i": "2"
+    }
+    // {
+    //   "x": 6,
+    //   "y": 0,
+    //   "w": 2,
+    //   "h": 3,
+    //   "i": "3"
+    // },
+    // {
+    //   "x": 8,
+    //   "y": 0,
+    //   "w": 2,
+    //   "h": 3,
+    //   "i": "4"
+    // },
+    // {
+    //   "x": 10,
+    //   "y": 0,
+    //   "w": 2,
+    //   "h": 3,
+    //   "i": "5"
+    // },
+    // {
+    //   "x": 0,
+    //   "y": 5,
+    //   "w": 2,
+    //   "h": 5,
+    //   "i": "6"
+    // },
+    // {
+    //   "x": 2,
+    //   "y": 5,
+    //   "w": 2,
+    //   "h": 5,
+    //   "i": "7"
+    // },
+    // {
+    //   "x": 4,
+    //   "y": 5,
+    //   "w": 2,
+    //   "h": 5,
+    //   "i": "8"
+    // },
+    // {
+    //   "x": 6,
+    //   "y": 4,
+    //   "w": 2,
+    //   "h": 4,
+    //   "i": "9"
+    // },
+    // {
+    //   "x": 8,
+    //   "y": 4,
+    //   "w": 2,
+    //   "h": 4,
+    //   "i": "10"
+    // },
+    // {
+    //   "x": 10,
+    //   "y": 4,
+    //   "w": 2,
+    //   "h": 4,
+    //   "i": "11"
+    // },
+    // {
+    //   "x": 0,
+    //   "y": 10,
+    //   "w": 2,
+    //   "h": 5,
+    //   "i": "12"
+    // },
+    // {
+    //   "x": 2,
+    //   "y": 10,
+    //   "w": 2,
+    //   "h": 5,
+    //   "i": "13"
+    // },
+    // {
+    //   "x": 4,
+    //   "y": 8,
+    //   "w": 2,
+    //   "h": 4,
+    //   "i": "14"
+    // },
+    // {
+    //   "x": 6,
+    //   "y": 8,
+    //   "w": 2,
+    //   "h": 4,
+    //   "i": "15"
+    // },
+    // {
+    //   "x": 8,
+    //   "y": 10,
+    //   "w": 2,
+    //   "h": 5,
+    //   "i": "16"
+    // },
+    // {
+    //   "x": 10,
+    //   "y": 4,
+    //   "w": 2,
+    //   "h": 2,
+    //   "i": "17"
+    // },
+    // {
+    //   "x": 0,
+    //   "y": 9,
+    //   "w": 2,
+    //   "h": 3,
+    //   "i": "18"
+    // },
+    // {
+    //   "x": 2,
+    //   "y": 6,
+    //   "w": 2,
+    //   "h": 2,
+    //   "i": "19"
+    // }
+  ];
+  export default {
+    name: 'DashboardGrid',
+    components: {
+      GridLayout,
+      GridItem,
+      MyChart
+    },
+    props: {},
+    data() {
+      return {
+        layout: testLayout,
+        draggable: true,
+        resizable: true
+      };
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+  };
+</script>
+<style lang="scss" scoped>
+  .grid {
+    height: 100%;
+    width: 100%;
+
+    &-title {
+      border: 1px solid #000;
+      text-align: left;
+
+      &-item {
+        width: 180px;
+        display: inline-block;
+      }
+    }
+  }
+
+  .vue-grid-item {
+    border: 1px solid #E0E0E0;
+  }
+  .item-charts{
+    width: 100%;
+    height: 100%;
+    padding: 0 10px 10px;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/components/MyChart.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="chart">
+    <div class="chart-title">panel{{id}}</div>
+    <chart :options="options" autoresize></chart></div>
+</template>
+<script>
+import chart from 'vue-echarts';
+import "echarts/lib/chart/line";
+
+export default {
+  name: 'MyChart',
+  components: {chart},
+  props: {
+    id:{
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      options: {
+        grid:{
+          left: 40,
+          right: 0,
+          top: 10,
+          bottom: 20
+        },
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+    }]
+}
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="scss" scoped>
+.chart{
+  width: 100%;
+  height: 100%;
+  &-title{
+    height: 28px;
+    line-height: 28px;
+    cursor: move;
+    
+  }
+}
+.echarts {
+  width: 100%;
+  height: calc(100% - 28px);
+}
+</style>
