df3aa33dcd5fa6d1539151951351319aedba6665
chenxch <chenxch@ffcs.cn>
2020-07-26 23:54:50 +0800

    微前端

--- src/main.js
+++ src/main.js
@@ -6,6 +6,31 @@ import App from './App.vue'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
-new Vue({
+let instance = null
+
+  function render () {
+    instance = new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+  }
+
+  if (!window.__POWERED_BY_QIANKUN__) {
+    render()
+  }
+
+  export async function bootstrap () {
+    // eslint-disable-next-line no-console
+    console.log('vue app bootstraped')
+  }
+
+  export async function mount (props) {
+    // eslint-disable-next-line no-console
+    console.log('props from main framework', props)
+    render()
+  }
+
+  export async function unmount () {
+    instance.$destroy()
+    instance = null
+  }
